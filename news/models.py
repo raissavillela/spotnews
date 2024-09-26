404ad6df980f4e2e8f3cff426794a4e53966d7b5
Raissa Villela <34253483+raissavillela@users.noreply.github.com>
2024-09-26 12:36:53 -0700

    Requisito 3

--- news/models.py
+++ news/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.utils import timezone
 
 
 class Category(models.Model):
@@ -16,3 +18,24 @@ class User(models.Model):
 
     def __str__(self):
         return self.name
+
+
+def validate_title(value):
+    if len(value.split()) < 2:
+        raise ValidationError('O título deve conter pelo menos 2 palavras.')
+
+
+class News(models.Model):
+    title = models.CharField(
+        max_length=200,
+        blank=False,
+        null=False,
+        validators=[validate_title])
+    content = models.TextField(blank=False, null=False)
+    author = models.ForeignKey('User', on_delete=models.CASCADE)
+    created_at = models.DateField(default=timezone.now)
+    image = models.ImageField(upload_to='img/', blank=True, null=True)
+    categories = models.ManyToManyField('Category', blank=False)
+
+    def __str__(self):
+        return self.title
