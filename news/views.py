47c91155ae723f4f59d29dc1c41575303986a087
Raissa Villela <34253483+raissavillela@users.noreply.github.com>
2024-09-26 15:13:18 -0700

    Requisito 4

--- news/views.py
+++ news/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import News
 
-# Create your views here.
+
+def index(request):
+    news_list = News.objects.all()
+    return render(request, 'index.html', {'news_list': news_list})
+
+
+def news_detail(request, news_id):
+    news_item = get_object_or_404(News, pk=news_id)
+    return render(request, 'news_detail.html', {'news_item': news_item})
+
+
+def home_page(request):
+    news_list = News.objects.all()
+    return render(request, 'home.html', {'news_list': news_list})
